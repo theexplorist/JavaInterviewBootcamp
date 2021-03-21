0bdd84c0b2aa1c16fffdb7021f17a3bd484ce6c8
K007 <36664705+theexplorist@users.noreply.github.com>
2021-03-21 22:58:55 +0530

    Create ProductLessThanK.java

---
+++ Day1/ProductLessThanK.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+     
+        int ans = 0;
+        int prod = 1;
+        // Two pointers, and expanding by inc end
+        for(int start = 0, end = 0; end < nums.length; end++) {
+            
+            prod *= nums[end]; //prod = prod * nums[end]
+            
+            //when we will contract the window?
+            while(start <= end && prod >= k) {
+                //contract
+                //Throw out unwanted number 
+                prod = prod / nums[start];
+                //contract
+                start++;
+            }
+            
+            //valid subarray count in current window
+            ans += (end - start + 1); //length of the window
+        }
+        return ans;
+    }
+}
